53d2d65d01a68daeb7230c95e644d4fbd91b02c2
Jean-Benoit Griesner <jb.griesner@qwant.com>
2019-09-29 04:32:18 +0200

    max autonomy

--- src/millenium_falcon_onboard_computer/falcon.rs
+++ src/millenium_falcon_onboard_computer/falcon.rs
@@ -40,12 +40,12 @@ fn init_seen_planets(galaxy: &Galaxy, cd: u32) -> HashMap<(u32,u32),bool> {
 pub fn generate_paths(galaxy: &Galaxy, src: String, dst: String, cd: u32, autonomy: u32) -> Vec<Path> {
     let mut all_paths: Vec<Path> = Vec::new();
 
-    fn visit(time: u32, src: u32, dst: u32, galaxy: &Galaxy, path: &mut Path, all_paths: &mut Vec<Path>, seen: &mut HashMap<(u32,u32),bool>, cd: u32, autonomy: u32) {
+    fn visit(time: u32, src: u32, dst: u32, galaxy: &Galaxy, path: &mut Path, all_paths: &mut Vec<Path>, seen: &mut HashMap<(u32,u32),bool>, cd: u32, autonomy: u32, dist: u32, MAX_AUTONOMY: u32) {
         println!("\nFalcon (autonomy: {}) visiting planet: {}, cd: {}, at time: {}, through path: {:?}", autonomy, src, cd, time, path);
         seen.entry((src,time)).and_modify(|e| { *e = true });
         path.add_planet_time(src, time);
 
-        let time = time + 1;
+        let time = time + dist;
 
         if time <= cd {
             if src == dst {
@@ -53,18 +53,18 @@ pub fn generate_paths(galaxy: &Galaxy, src: String, dst: String, cd: u32, autono
             } else {
                 for (planet, distance) in galaxy.planets.get(&src).unwrap().neighbors.iter() {
 
-                    println!("candidate planet: {:?}, distance: {:?}", *planet, distance);
-                    println!("neighbors: {:?}", galaxy.planets.get(&src).unwrap().neighbors);
-                    println!("seen_planets: {:?}", seen);
-                    println!("COND1: {:?}", seen.get(&(*planet,time)).unwrap());
-                    println!("COND2: {:?}", *distance < autonomy);
+                    println!("\tcandidate planet: {:?}, distance: {:?}", *planet, distance);
+                    println!("\tneighbors: {:?}", galaxy.planets.get(planet).unwrap().neighbors);
+                    //println!("\tseen_planets: {:?}", seen);
+                    println!("\tCOND1: {:?}", seen.get(&(*planet,time)).unwrap());
+                    println!("\tCOND2: {:?}", *distance < autonomy);
 
                     //println!("seen: {:?} planet: {}, time: {}, cd: {}", seen, *planet, time, cd);
                     if !seen.get(&(*planet,time)).unwrap() && *distance <= autonomy {
                         if *planet == src {
-                            visit(time, *planet, dst, galaxy, path, all_paths, seen, cd, autonomy);
+                            visit(time, *planet, dst, galaxy, path, all_paths, seen, cd, MAX_AUTONOMY, *distance, MAX_AUTONOMY);
                         } else {
-                            visit(time, *planet, dst, galaxy, path, all_paths, seen, cd, autonomy - distance);
+                            visit(time, *planet, dst, galaxy, path, all_paths, seen, cd, autonomy - distance, *distance, MAX_AUTONOMY);
                         }
                     }
                 }
@@ -75,6 +75,7 @@ pub fn generate_paths(galaxy: &Galaxy, src: String, dst: String, cd: u32, autono
     };
 
     let time = 0;
+    let MAX_AUTONOMY: u32 = autonomy;
 
     let mut seen_planets = init_seen_planets(galaxy, cd+1);
 
@@ -83,7 +84,7 @@ pub fn generate_paths(galaxy: &Galaxy, src: String, dst: String, cd: u32, autono
 
     let mut path = Path::new();
 
-    visit(time, *src_id, *dst_id, galaxy, &mut path, &mut all_paths, &mut seen_planets, cd, autonomy);
+    visit(time, *src_id, *dst_id, galaxy, &mut path, &mut all_paths, &mut seen_planets, cd, autonomy, 1, MAX_AUTONOMY);
 
     all_paths
 }
